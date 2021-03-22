d2c2260ca30ea646310008174396be0a682f89d1
Anh Vu <vu.haianh291@gmail.com>
2021-03-22 16:57:48 +0200

    Feat: Add class base views

--- cbvSerializers/cbvApp/views.py
+++ cbvSerializers/cbvApp/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from cbvApp.models import Student
+from cbvApp.serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class StudentList(APIView):
+
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = StudentSerializer(data=request.data)
+        if data.is_valid():
+            data.save()
+            return Response(data.data, status=status.HTTP_201_CREATED)
+        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StudentDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    def delete(self, pk, request):
+        student = self.get_object(pk)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, pk, request):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
