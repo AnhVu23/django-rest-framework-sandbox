312f5a044995352fbc50b9874fb33da31318c3dd
Anh Vu <vu.haianh291@gmail.com>
2021-03-22 16:22:41 +0200

    Fix: Import

--- fbvSerializers/fbvApp/views.py
+++ fbvSerializers/fbvApp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from fbvApp.models import Student
 from fbvApp.serializers import StudentSerializer
-from rest_framework import Response, status, api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
 @api_view(['GET', 'POST'])
 def student_list(request):
     if request.method == 'GET':
-        students = Student.object.all()
+        students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
