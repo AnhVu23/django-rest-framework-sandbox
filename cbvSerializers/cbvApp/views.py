3492775af602b8b535d54f157bd5905c9bfe58e4
Anh Vu <vu.haianh291@gmail.com>
2021-03-23 10:25:45 +0200

    Feat: Using mixin

--- cbvSerializers/cbvApp/views.py
+++ cbvSerializers/cbvApp/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 from cbvApp.models import Student
 from cbvApp.serializers import StudentSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from rest_framework.views import APIView
+
+class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request):
+        return self.list(request=request)
+
+    def post(self, request):
+        return self.create(request)
+
+class StudentDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request):
+        return self.delete(request, pk)
+
 
 
 # Create your views here.
